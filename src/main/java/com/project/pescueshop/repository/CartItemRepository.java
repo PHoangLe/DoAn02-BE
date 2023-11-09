a2be70e0325015bec5dc16b7031af086df369f49
PHoangLe <lehoangphuc180502@gmail.com>
2023-11-09 23:18:09 +0700

    update cart api

--- src/main/java/com/project/pescueshop/repository/CartItemRepository.java
+++ src/main/java/com/project/pescueshop/repository/CartItemRepository.java
@@ -2,6 +2,8 @@ package com.project.pescueshop.repository;
 
 import com.project.pescueshop.model.entity.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CartItemRepository extends JpaRepository<CartItem, String> {
 }
